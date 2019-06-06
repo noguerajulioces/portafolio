e189a9d2cbd0a128a4219510f0142068b77f7e8a
Julio Noguera <noguerajulioces@MacBook-Air-de-Julio.local>
2019-06-06 01:22:04 -0500

    edit controllers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.all
-    @projects = Project.all
+    @articles = Article.all.order("created_at DESC")
+    @projects = Project.all.order("created_at DESC")
   end
 end
